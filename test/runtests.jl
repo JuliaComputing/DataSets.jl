43aa04b56900deb35f8d38d7775a3282cb1c077a
Chris Foster <chris42f@gmail.com>
2020-10-28 14:52:41 +1000

    Add some basic tests for data entry points

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,28 @@
 using DataSets
 using Test
 
-@testset "DataSets.jl" begin
-    # Write your tests here.
+#--------------------------------------------------
+
+read_data = nothing
+
+@datafunc function main1(x::Blob=>String, t::Tree=>FileTree)
+    csv_data = open(io->read(io,String), t["1.csv"])
+    global read_data = (x_string=x, csv_data=csv_data)
+end
+
+@datafunc function main1(x::Blob=>IO)
+    x_data = read(x, String)
+    global read_data = (x_data=x_data,)
+end
+
+proj = DataSets.load_project("Data.toml")
+
+@testset "@datafunc and @datarun" begin
+    @datarun proj main1("a_text_file", "a_tree_example")
+
+    @test read_data == (x_string="Some text file!\n",
+                        csv_data="Name,Age\n\"Aaron\",23\n\"Harry\",42\n")
+
+    @datarun proj main1("a_text_file")
+    @test read_data == (x_data="Some text file!\n",)
 end
