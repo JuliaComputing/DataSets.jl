ad3c9a9f42b2de275b04bb7ddb3a3ea69791424a
Morten Piibeleht <morten.piibeleht@gmail.com>
2022-11-01 09:33:29 +1300

    Fix nightly tests (#50)
    * Use Pkg.develop instead of LOAD_PATH
    
    * Expand CI test matrix to include 1.5, 1.7
    
    1.5 is the lowest version we support here.

--- test/driver_autoload.jl
+++ test/driver_autoload.jl
@@ -1,6 +1,6 @@
 @testset "Automatic code loading for drivers" begin
     empty!(DataSets.PROJECT)
-    pushfirst!(LOAD_PATH, abspath("drivers"))
+    Pkg.develop(path=joinpath(@__DIR__, "drivers", "DummyStorageBackends"))
     ENV["JULIA_DATASETS_PATH"] = joinpath(@__DIR__, "DriverAutoloadData.toml")
     DataSets.__init__()
     @test haskey(DataSets._storage_drivers, "DummyTomlStorage")
